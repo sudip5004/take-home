e11390e186e827511b4ce9a9262a0857e2657592
sudip5004 <126877641+sudip5004@users.noreply.github.com>
2023-03-07 21:39:24 +0530

    Update GameTests.java
    added test  case

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,58 @@
 package fun;
 
-public class GameTests {
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GameTests {
+	@Test
+	public void testNoGameNameInput() {
+		String response = invokeAPI("http://localhost:8080/game?name=");
+		assertTrue(response.toString().contains("Playing Sudoku is fun!"));
+	}
+
+	@Test
+	public void testGameNameInput() {
+		String response = invokeAPI("http://localhost:8080/game?name=Chess");
+		assertTrue(response.toString().contains("Playing Chess is fun!"));
+
+	}
+
+	@Test
+	public void validateWithJsonPath() {
+		String json = invokeAPI("http://localhost:8080/game?name=testing");
+
+		List<String> value = JsonPath.read(json, "$..text");
+		assertTrue(value.get(0).contains("Playing"));
+	}
+
+	@Test
+	public void validateJsonParsing() throws JsonParseException, JsonMappingException, IOException {
+		String json = invokeAPI("http://localhost:8080/game?name=Chess");
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		GameResponse gameResponse = objectMapper.readValue(json, GameResponse.class);
+
+		assertTrue(gameResponse.getText().contains("Playing"));
+	}
+
+	private static String invokeAPI(String urlString) {
+		RestTemplate rt= new RestTemplate();
+		String response = rt.getForObject(urlString, String.class);
+		return response;
+	}
 
 }
